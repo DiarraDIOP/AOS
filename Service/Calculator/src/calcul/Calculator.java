cf302114300bc290f3ce1346220980082c60a43e
DiarraDIOP <sokhna.d.diop@gmail.com>
2015-10-28 23:03:26 +0100

    Première version du projet

---
+++ Service/Calculator/src/calcul/Calculator.java
@@ -0,0 +1,20 @@
+package calcul;
+
+public class Calculator {
+
+	public int addition(int a, int b){
+		return (a+b);
+	}
+	
+	public int soustraction(int a, int b){
+		return (a-b);
+	}
+	
+	public int multiplication(int a, int b){
+		return (a*b);
+	}
+	
+	public int division(int a, int b){
+		return (a/b);
+	}
+}

5ffcb8181f2599c8a453956ab6adc1a25cc14ea8
DiarraDIOP <sokhna.d.diop@gmail.com>
2015-10-31 00:39:49 +0100

    Ajout de la fonctionnalité de téléchargement de fichier zip depuis une URL

--- Service/amel/client.php
+++ Service/amel/client.php
@@ -1,8 +1,16 @@
 <?php
+
 include("lib/nusoap.php");
-$client = new soapclient("http://localhost/WebServiceSoap/server.php?wsdl");
-$result    =    $client->gethelloworld("Milap Patel");
-echo "<pre>";
-print_r($result);
-echo "</pre>";
+
+	file_put_contents("loto.zip", file_get_contents("https://media.fdj.fr/generated/game/loto/nouveau_loto.zip"));
+      
+	
+/*
+	exemple
+	include("lib/nusoap.php");
+	$client = new soapclient("http://localhost/WebServiceSOAP/server.php?wsdl");
+	$result    =    $client->gethelloworld("Milap Patel");
+	echo "<pre>";
+	print_r($result);
+	echo "</pre>";*/
 ?>

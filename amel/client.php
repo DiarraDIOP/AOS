7c5831fa7bc604115600d9c5b9b812a474a6eb96
DiarraDIOP <sokhna.d.diop@gmail.com>
2015-10-30 19:48:39 +0100

    MAJ

--- amel/client.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("lib/nusoap.php");
-$client = new soapclient("http://localhost/WebServiceSoap/server.php?wsdl");
-$result    =    $client->gethelloworld("Milap Patel");
-echo "<pre>";
-print_r($result);
-echo "</pre>";
-?>

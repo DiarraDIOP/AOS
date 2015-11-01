b83554617fecec388759162eee92001316871fb9
DiarraDIOP <sokhna.d.diop@gmail.com>
2015-11-01 01:12:00 +0100

    maj

--- Service/amel/index.php
+++ Service/amel/index.php
@@ -22,62 +22,48 @@
     </div>
     <div class="col-md-2">
         <ul class="nav nav-pills nav-stacked" style="width:200px; height:100px;">
-			<li role="presentation" class="active"><a href="#"><strong> <em>LOTO </em></strong> </a></li>
+			<li role="presentation" class="active"><a href="client.php"><strong> <em>LOTO </em></strong> </a></li>
 			<li role="presentation"><a href="#"><strong>Euro Million</strong></a></li>
 			<li role="presentation"><a href="#"><strong>Autres</strong></a></li>
 		</ul>
     </div>
+    
     <div class="col-md-10" align="center" >
-		<div class="container">
-			<div class="col-md-1 col-md-offset-1"><br>
-				<img class="img" src="images/numeros_loto.png" /><br><br><br>
-				<img class="img" src="images/Capture.png" /> 
-			</div>
-			
-			<div class="col-md-3">
-				<div class="input-group">
-							<input type="number" class="form-control" style="width:50px; height:50px;">
-							<input type="number" class="form-control" style="width:50px; height:50px;">
-							<input type="number" class="form-control" style="width:50px; height:50px;">
-							<input type="number" class="form-control" style="width:50px; height:50px;">
-							<input type="number" class="form-control" style="width:50px; height:50px;">
-							
-				</div><br>
-				<input type="number" class="form-control" style="width:50px; height:50px;">  </td>
-			</div>
-			
-			<div class="col-md-4">
-				<img src="images/loto.jpg" class="img-responsive img-circle" alt="Responsive image" style="-ms-transform: rotate(15deg); -webkit-transform: rotate(15deg);">
-			</div>
-	
-		</div>
-		<div class="container center"><br><br><br><br><br><br>
-		  <!-- BUTTON -->
-		  <button type="button" class="btn btn-warning btn-lg" data-toggle="modal" data-target="#myModal"> Testez votre ticket </button>
+      <form method="post" action="client.php">
+    		<div class="container">
+    			<div class="col-md-1 col-md-offset-1"><br>
+    				<img class="img" src="images/numeros_loto.png" /><br><br><br>
+    				<img class="img" src="images/Capture.png" /> 
+    			</div>
+    			
+    			<div class="col-md-3">
+                     
+    				<div class="input-group">
+                        
+    							<input type="number" name="numero1" class="form-control" style="width:50px; height:50px;">
+    							<input type="number" name="numero2" class="form-control" style="width:50px; height:50px;">
+    							<input type="number" name="numero3" class="form-control" style="width:50px; height:50px;">
+    							<input type="number" name="numero4" class="form-control" style="width:50px; height:50px;">
+    							<input type="number" name="numero5" class="form-control" style="width:50px; height:50px;">
 
-		  <!-- Modal -->
-		  <div class="modal fade" id="myModal" role="dialog">
-			<div class="modal-dialog">
-			  <!-- Modal content-->
-			  <div class="modal-content">
-					<div class="modal-header">
-					  <button type="button" class="close" data-dismiss="modal">&times;</button>
-					  <h4 class="modal-title">Modal Header</h4>
-					</div>
-					<div class="modal-body">
-					  <p>Some text in the modal.</p>
-					</div>
-					<div class="modal-footer">
-					  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-					</div>
-			  </div>
-			  
-			</div>
-		  </div>
-		  
-		</div>
-    </div>
-   
+
+
+    				</div><br>
+    				<input type="number" name="numeroChance" class="form-control" style="width:50px; height:50px;">  </td>
+    			</div>
+    			
+    			<div class="col-md-4">
+    				<img src="images/loto.png" class="img-responsive img-circle" alt="Responsive image" style="-ms-transform: rotate(15deg); -webkit-transform: rotate(15deg); height :100px; width:200px;"/>
+    			</div>
+    	
+    		</div>
+    		<div ><br><br><br><br><br><br>
+    		  <!-- BUTTON -->
+              <input type="submit" name="btn" value="Testez votre ticket" id="submitBtn" class="btn btn-warning btn-lg" />
+    		  
+    		</div>
+        </div>
+   </form>
   </div>
 </div>
       

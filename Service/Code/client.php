885a09f1a698c2be1377094ebe3dbf241bbc9352
DiarraDIOP <sokhna.d.diop@gmail.com>
2015-11-02 20:20:26 +0100

    ajout des conditions selon le jeu

--- Service/Code/client.php
+++ Service/Code/client.php
@@ -4,20 +4,31 @@ include("index.php");
 
 $client = new soapclient("http://localhost/AOS/Service/Code/server.php?wsdl");   
 
+if (isset($_GET['x'] )) {
 
-if (isset($_POST['numero1']) && isset($_POST['numero2']) && isset($_POST['numero3']) && isset($_POST['numero4']) 
-	&& isset($_POST['numero5']) && isset($_POST['numeroChance'])) {
+	if ($_GET['x'] == 'loto'){
 
-	
-	//appel du service
-	$res = $client->verifTicket("nouveau_loto", intval($_POST['numero1']), intval($_POST['numero2']), intval($_POST['numero3']), intval($_POST['numero4']), intval($_POST['numero5']), intval($_POST['numeroChance'])); 
+		if (isset($_POST['numero1']) && isset($_POST['numero2']) && isset($_POST['numero3']) && isset($_POST['numero4']) 
+			&& isset($_POST['numero5']) && isset($_POST['numeroChance'])) {
 
-	
-	if($res){
-		header('location: gagner.php'); 
-	}else{
-		header('location: perdu.php'); 
+			//appel du service
+			$res = $client->verifTicket("nouveau_loto", intval($_POST['numero1']), intval($_POST['numero2']), intval($_POST['numero3']), intval($_POST['numero4']), intval($_POST['numero5']), intval($_POST['numeroChance'])); 
+
+		
+			if($res){
+				header('location: gagner.php?x=loto'); 
+				
+			}else{
+				header('location: perdu.php?x=loto'); 
+			}
+		}
+	}elseif ($_GET['x'] == 'euro') {
+		# code...
+	}elseif (($_GET['x'] == 'joker')) {
+		# code...
 	}
 }
+
+
 
 ?>

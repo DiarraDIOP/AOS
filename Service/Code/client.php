2cb9e35fd071479485e6ec0a22e8151269f92c81
DiarraDIOP <sokhna.d.diop@gmail.com>
2015-11-03 20:05:11 +0100

    maj

--- Service/Code/client.php
+++ Service/Code/client.php
@@ -5,38 +5,6 @@ include("index.php");
 $client = new soapclient("http://localhost/AOS/Service/Code/server.php?wsdl");   
 
 
-		function recupereJoker($name){
-
-		//téléchargement du fichier depuis le lien du site de la FDJ
-		file_put_contents($name.".zip", file_get_contents("https://media.fdj.fr/generated/game/jokerplus/".$name.".zip"));
-	      
-	    //le dézipper
-		$zip = new ZipArchive;
-		$res = $zip->open($name.".zip");
-		if ( $res === TRUE) {
-		    $zip->extractTo('./');
-		    $zip->close();
-		} else {
-		    var_dump($res);
-		}
-
-		//Récuperation de la deuxième 
-		$ligne = 1; // compteur de lignes
-		$resultats = array();
-		$fic = fopen($name.".csv", "a+");
-		while($tab=fgetcsv($fic,1024,';'))
-		{
-			$champs = count($tab);  
-			$ligne ++;
-			//affichage de le 2ème ligne
-			if ($ligne >2 && $ligne <4){
-				//affichage de la 5ème colonne
-					$resultats[0] = $tab[4];
-			}
-		}
-		return $resultats;
-	}
-
 
 if (isset($_GET['x'] )) {
 
